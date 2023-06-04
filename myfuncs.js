e528d3dab817d4cfe7b8c71e77e73a6c4268c585
PavanButke <butkepavan55@gmail.com>
2023-06-05 00:43:52 +0530

    funcs and map

---
+++ myfuncs.js
@@ -0,0 +1,11 @@
+function addTwo(num) {
+    return num + 2;
+}
+function signupUser(username, email, password) {
+    if (password === void 0) { password = ""; }
+}
+function login(username, email, password) { }
+signupUser("p", "pgb@gmail.com");
+addTwo(5); //gives error , needs number only
+
+
--- myfuncs.ts
+++ myfuncs.ts
@@ -3,4 +3,26 @@ function addTwo(num : number) //type casing
     return num+2;
 }
 
+function signupUser(username:string , email:string , password:string="")
+{}
+
+function login(username:string , email:string , password:string)
+{}
+
+
+signupUser("p","pgb@gmail.com");
+
 addTwo(5); //gives error , needs number only
+
+const getGreets=(s: string ):string =>{
+    return ""
+}
+
+//const heros= ["karn","dashgriva","gautama"]
+const heros=[1,2,3]
+
+heros.map((hero):string=>{
+    return `hero is ${hero}`
+    // return 1;
+})
+     
