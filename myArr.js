bcc2711e66ebe37c618446aa31008dbbc7deed09
PavanButke <butkepavan55@gmail.com>
2023-06-10 23:31:40 +0530

    arr

---
+++ myArr.js
@@ -0,0 +1,6 @@
+var roleNos = [];
+var names = [];
+var grades = [];
+var allMarks = [];
+roleNos.push(2, 23, 22);
+allMarks.push({ subject: "Maths", marks: 99 });
---
+++ myArr.ts
@@ -0,0 +1,13 @@
+const roleNos: number[]=[]
+const names: string[]=[]
+const grades: Array<number>=[]
+
+type subjectScore={
+    subject:string,
+    marks: number
+}
+
+const allMarks: subjectScore[]=[]
+
+roleNos.push(2,23,22);
+allMarks.push({subject:"Maths", marks:99})
