eb8450a593c6f6a09c421ae730e06e004dc020e1
PavanButke <butkepavan55@gmail.com>
2023-06-11 14:16:52 +0530

    reopening myInterface

--- myInterface.ts
+++ myInterface.ts
@@ -5,6 +5,11 @@ interface myProfile{
     salary?: number,
     myAge(): number
 }
+
+//reopenning interface myProfile
+interface myProfile{
+    authToken: string
+}
 const pbProfile={
     name:"Pavan",
     email:"butkepavan55",
@@ -12,7 +17,8 @@ const pbProfile={
     myAge:()=>
     {
         return 24;
-    }
+    },
+    authToken: "gmfmglknfg#$lfmgk45fle"
 }
 
 export {}
