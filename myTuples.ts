8e1ad985eb53daad107c7a956dd80438f6439362
PavanButke <butkepavan55@gmail.com>
2023-06-11 13:35:59 +0530

    tuples

---
+++ myTuples.ts
@@ -0,0 +1,6 @@
+//very script in case of no. of ele defined == no.of decl
+let direction:[string , string , string , string]=["north","south","east","west" , "northwest"];
+//cannot use the below one: gives Type '[string, string, string, string, string]' is not assignable to type '[string, string, string, string]'.ts
+let directions:[string , string , string , string]=["north","south","east","west" , "northwest"];
+
+export {}   
